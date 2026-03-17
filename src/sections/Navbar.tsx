45c37666dade0c68ac878bd636510e1132a6ce82
micaren321-byte <mica.ren321@gmail.com>
2026-03-17 09:21:05 +0800

    Update Navbar.tsx

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Navbar() {
                 isScrolled ? 'text-[#3A5F40]' : 'text-white'
               }`}
             >
-              职芽求职
+              职芽1111
             </span>
           </a>
 
